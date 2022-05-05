3ef44fabedf19ad518cddf84b465d84a19843bd0
Steffen568 <sgonzalez568@gmail.com>
2022-05-05 18:12:37 -0500

    added handlebars and populated homepage with test seeds

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 // collect packaged group of api endpoints and prefex tehm
 const router = require('express').Router();
+const homeRoutes = require('./home-routes')
 
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes)
 
 router.use((req, res) => {
   res.status(404).end();
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// path for css stylesheet
+const path = require('path')
+// importing handlebars
+const exphbs = require('express-handlebars');
+const exp = require('constants');
+const hbs = exphbs.create({})
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// cdd route using static keyward
+app.use(express.static(path.join(__dirname, 'public')));
+// handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
