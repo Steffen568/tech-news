5cd88ac510a66455da93ea05abe80156505b7cb7
Steffen568 <sgonzalez568@gmail.com>
2022-05-05 11:45:39 -0500

    hashed passwords

--- models/User.js
+++ models/User.js
@@ -1,52 +1,65 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 // create our User model / user inherits all functionality of the model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
-    // TABLE COLUMN DEFINITIONS GO HERE
-    {
-        id: {
-            // use the special sequelize dataTypes object provide what type of data it is
-            type: DataTypes.INTEGER,
-            //  this is the equivalent of sql's NOT NULL 
-            allowNull: false,
-            //  instruct that this is a primary key
-            primaryKey: true,
-            // turn on auto incremenmt
-            autoIncrement: true
-        },
-        // define a username column
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        // define email column
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            // there cannot be any duplicate email values in this table
-            unique: true,
-            // if allowNull is set to false, we can run our data through validators before creating table data
-            validate: {
-                isEmail: true
-            }
-        },
-        // define password column
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                // password must be at least four chars long
-                len: [4]
-            }
-        }
-    },
+      // TABLE COLUMN DEFINITIONS GO HERE
   {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
+    id: {
+      // use the special sequelize dataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      //  this is the equivalent of sql's NOT NULL
+      allowNull: false,
+      //  instruct that this is a primary key
+      primaryKey: true,
+      // turn on auto incremenmt
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // there cannot be any duplicate email values in this table
+      unique: true,
+      // if allowNull is set to false, we can run our data through validators before creating table data
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // password must be at least four chars long
+        len: [4]
+      }
+    }
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
